f7159c099fc2dabc93e0b7a2fbde29a5f7912b55
karson <73568290+vitekarson@users.noreply.github.com>
2021-11-16 13:22:53 +0800

    Add files via upload

---
+++ storeage/list/localStroageWhiteList.js
@@ -0,0 +1,14 @@
+/**
+ * localStorage error message
+ */
+export const ERROR_LOCAL_STORAGE_MESSAGE = 'localStorage.setItem: 当前 ${key} 未配置白名单此操作不生效, 请到 stroage/list/localStorageWhiteList.js 文件内配置'
+
+
+export const LOCAL_STORAGE_WHITE_LIST = [
+  /**
+   * @key .....
+   * @desc
+   */
+  'karson',
+
+]
---
+++ storeage/list/sessionStroageWhiteList.js
@@ -0,0 +1,8 @@
+/**
+ * localStorage error message
+ */
+export const ERROR_SESSION_STORAGE_MESSAGE = 'sessionStorage.setItem: 当前 ${key} 未配置白名单此操作不生效, 请到 storage/list/sessionStorageWhiteList.js 文件内配置'
+
+export const SESSION_STORAGE_WHITE_LIST = [
+  'karson'
+]
