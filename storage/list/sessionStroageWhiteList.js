29709d7bd2d8bf09c91b5f916e398d66fa0756e0
karson <73568290+vitekarson@users.noreply.github.com>
2021-11-16 14:17:58 +0800

    Update sessionStroageWhiteList.js

--- storage/list/sessionStroageWhiteList.js
+++ storage/list/sessionStroageWhiteList.js
@@ -1,5 +1,5 @@
 /**
- * localStorage error message
+ * sessionStorage error message
  * 自定义即可
  */
 export const ERROR_SESSION_STORAGE_MESSAGE = 'sessionStorage.setItem: 当前 ${key} 未配置白名单此操作不生效, 请到 storage/list/sessionStorageWhiteList.js 文件内配置'
